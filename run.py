d3f63aa04feb63f10df5ecdb3226c2c698f280fe
Collin Murphy <collinmuurphy@gmail.com>
2022-10-28 15:07:11 -0700

    working for 1 argument

--- run.py
+++ run.py
@@ -7,23 +7,34 @@ argcount = len(sys.argv)
 
 if argcount <= 1:
     print('Use command "python3 run.py --usage" for help.')
-    exit
+    exit()
 elif sys.argv[1] == "--usage":
     print("Usage: python3 run.py <filename.py> <args>")
-    exit
+    exit()
+elif argcount == 2:
+    print("Not enough arguments")
+    exit()
 else:
     fname = sys.argv[1]
     args = sys.argv[2:]
-    print(f"Filename = {fname}")
-    print(f"Args = {args}")
+    print(f"Filename   = {fname}")
+    print(f"Argunments = {args}\n")
     
 if fname in os.listdir('.'):
     module = import_module(fname.split('.')[0])
+    Solution = getattr(module, dir(module)[0])
+    sol = Solution()
+    func = getattr(sol, [f for f in dir(sol) if f.startswith("__") == False][0]) #if callable(f)])
+    print("Output:")
+    func(args[0])
+else:
+    print("File not found")
 
-#module.Solution().isPalindrome(args[0])
 
-classes = [x for x in dir(module) if inspect.isclass(getattr(module, x))]
-functions = dir(classes[0])
-print([f for f in functions if "__" in f])
+#### OLD CODE ###
+#print(getattr(dir(module)[0], 'isPalindrome'))
+#print(f"functions = {functions}")
+#print([f for f in inspect.getmembers(classes[0], predicate=inspect.isfunction)]) #if callable(f)])
+#print(classes[0].stream
 #function = getattr(module, classes[0], )
 #print(function)
