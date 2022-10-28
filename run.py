ea7785c576dde0a06875844b42a4763f66cceee3
Collin Murphy <collinmuurphy@gmail.com>
2022-10-28 16:14:03 -0700

    added solutions directory

--- run.py
+++ run.py
@@ -4,39 +4,46 @@ import os
 from importlib import import_module
 import inspect
 
-argcount = len(sys.argv)
+def main():
+    directory = 'solutions'
+    argcount = len(sys.argv)
 
-#check if arguments are correct
-if argcount <= 1:
-    print('Use command "python3 run.py --usage" for help.')
-    exit()
-elif sys.argv[1] == "--usage":
-    print("Usage: python3 run.py <filename.py> <args>")
-    exit()
-elif argcount == 2:
-    print("Not enough arguments")
-    exit()
-else:
-    fname = sys.argv[1]
-    args = sys.argv[2:]
-    print(f"Filename   = {fname}")
-    print(f"Argunments = {args}\n")
-    
-#What to do if the correct amount of arguments are correct
-#check if file exists
-if fname in os.listdir('.'):
-    #get module name
-    module = import_module(fname.split('.')[0])
-    
-    #get first class in file
-    Solution = getattr(module, dir(module)[0])
-    
-    #create instance of class
-    sol = Solution()
+    #check if arguments are correct
+    if argcount <= 1:
+        print('Use command "./run.py --usage" for help.')
+        exit()
+    elif sys.argv[1] == "--usage":
+        print('Usage: ./run.py <filename.py> <args>\nStore solutions in the "{directory}/" directory')
+        exit()
+    elif argcount == 2:
+        print('Not enough arguments')
+        exit()
+    else:
+        path = sys.argv[1]
+        args = sys.argv[2:]
+        print(f'File path  = {path}')
+        print(f'Arguments = {args}\n')
+        
+    #What to do if the correct amount of arguments are correct
+    #check if file exists
+    fname = ('./' + path).split('/')[-1] 
+    if fname in os.listdir(directory):
+        #get module name (the '.' splits the direcories instead of '/')
+        module = import_module(directory + '.' + fname.split('.')[0])
 
-    #get first function and call it
-    func = getattr(sol, [f for f in dir(sol) if f.startswith("__") == False][0]) #if callable(f)])
-    print("Output:")
-    func(args[0])
-else:
-    print("File not found")
+        #get first class in file
+        Solution = getattr(module, dir(module)[0])
+        
+        #create instance of class
+        sol = Solution()
+
+        #get first function and call it
+        func = getattr(sol, [f for f in dir(sol) if f.startswith('__') == False][0]) #if callable(f)])
+        print('Output:')
+        func(args[0])
+        
+    else:
+        print(f'File not found in "{directory}/" directory.')
+
+if __name__ == "__main__":
+    main()
